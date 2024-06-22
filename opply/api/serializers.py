e5fcad54a1515563e5013f2cf5b6e99e2c412399
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-06-22 08:55:09 +0100

    Initial ModelSerializer implementations

--- opply/api/serializers.py
+++ opply/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from opply.api.models import Order, OrderProductQuantity, Product
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,27 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
+
+class OrderProductQuantitySerializer(serializers.HyperlinkedModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderProductQuantity
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    order_product_quantity = OrderProductQuantitySerializer()
+
+    class Meta:
+        model = Order
+        fields = ['order_date', 'order_product_quantity']
+
+
