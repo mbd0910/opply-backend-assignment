153ce4894d1057800ddde42c6047a9e4172996e9
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-06-22 11:47:59 +0100

    Logic for POST Order v1

--- opply/api/serializers.py
+++ opply/api/serializers.py
@@ -33,11 +33,27 @@ class OrderProductQuantitySerializer(serializers.HyperlinkedModelSerializer):
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer()
+    user = serializers.SerializerMethodField()
     product_quantities = OrderProductQuantitySerializer(many=True)
 
     class Meta:
         model = Order
         fields = ['user', 'order_date', 'product_quantities']
 
+    def create(self, validated_data):
+        product_quantities_data = validated_data.pop('product_quantities')
+        order = Order.objects.create(**validated_data)
+        for product_quantity in product_quantities_data:
+            OrderProductQuantity.objects.create(order=order, **product_quantity)
+            product = product_quantity['product']
+            quantity = product_quantity['quantity']
+            product.stock -= quantity
+            product.save()
+        return order
+
+    def get_user(self, obj):
+        if self.context.get('request').method == 'GET':
+            return UserSerializer(obj.user).data
+        # Use PrimaryKeyRelatedField for POST requests so we only have to provide an ID
+        return obj.user.id
 
--- opply/api/views.py
+++ opply/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import Group, User
 from opply.api.models import Product, Order
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from opply.api.serializers import ProductInventorySerializer, OrderSerializer
@@ -15,6 +16,22 @@ class ProductViewSet(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        product_quantities = serializer.validated_data['product_quantities']
+        for product_quantity in product_quantities:
+            product = product_quantity['product']
+            quantity = product_quantity['quantity']
+            if quantity > product.stock:
+                return Response({'error': f"Only {product.stock} units of {product.name} available."},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
